8091a96e948bb561a6833ef300c22957722f2788
KaANO-8 <kaanorahul8@gmail.com>
2022-07-17 17:28:49 +0530

    fixed nav host fragment

--- app/src/main/java/com/kaano8/androidcore/main/MainActivity.kt
+++ app/src/main/java/com/kaano8/androidcore/main/MainActivity.kt
@@ -2,9 +2,11 @@ package com.kaano8.androidcore.com.kaano8.androidcore.main
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.kaano8.androidcore.R
 import com.kaano8.androidcore.databinding.ActivityMainBinding
 import com.kaano8.androidcore.service.MemoFragment
 import com.kaano8.androidcore.service.ServiceViewModel
@@ -19,46 +21,24 @@ class MainActivity : AppCompatActivity() {
 
     private val serviceViewModel: ServiceViewModel by viewModels()
 
+    private lateinit var navHostFragment: NavHostFragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        Log.d(TAG, "onCreate: called")
-        Log.d(TAG, "onCreate: height = ${binding.recyclerView.height}")
+        navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        setupActionBarWithNavController(navController = navHostFragment.navController)
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d(TAG, "onStart: height = ${binding.recyclerView.height}")
-
+    override fun onSupportNavigateUp(): Boolean {
+        return navHostFragment.navController.navigateUp() || super.onSupportNavigateUp()
     }
 
-    override fun onResume() {
-        super.onResume()
-        Log.d(TAG, "onResume: height = ${binding.recyclerView.height}")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d(TAG, "onPause: called")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d(TAG, "onStop: ")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d(TAG, "onDestroy: ")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d(TAG, "onRestart: ")
-    }
-
+    /**
+     * Poor setup, will be constantly calling onPause - onResume every secs when broadcast is received
+     */
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
         val elapsedTime = intent?.getIntExtra(MemoFragment.NOTIFICATION_TEXT, 0) ?: 0
