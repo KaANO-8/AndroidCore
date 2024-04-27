447116773da6c6770d99aff8e491bbf1f7f81438
rahul.kanojia <kaanorahul8@gmail.com>
2024-04-27 14:47:43 +0530

    editted task

--- app/src/main/java/com/kaano8/androidcore/filedownloader/adapter/FileDownloadViewHolder.kt
+++ app/src/main/java/com/kaano8/androidcore/filedownloader/adapter/FileDownloadViewHolder.kt
@@ -1,18 +1,17 @@
 package com.kaano8.androidcore.com.kaano8.androidcore.filedownloader.adapter
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.kaano8.androidcore.com.kaano8.androidcore.filedownloader.repository.model.DownloadTask
 import com.kaano8.androidcore.databinding.DownloadListItemBinding
-import java.util.zip.Inflater
 
 class FileDownloadViewHolder(private val binding: DownloadListItemBinding): RecyclerView.ViewHolder(binding.root) {
 
     fun bind(downloadTask: DownloadTask, onPause: (Int) -> Unit, onStop: (Int) -> Unit) {
         with(binding) {
             progressBar.progress = downloadTask.progress
+            progressBar.max = downloadTask.time
             pauseButton.setOnClickListener { onPause(downloadTask.id) }
             stopButton.setOnClickListener { onStop(downloadTask.id) }
         }
