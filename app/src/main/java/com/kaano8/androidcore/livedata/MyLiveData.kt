958ca42c7facb53280f403c4287d9fca30e6240b
rahul.kanojia <kaanorahul8@gmail.com>
2024-04-30 11:04:04 +0530

    updated live data

--- app/src/main/java/com/kaano8/androidcore/livedata/MyLiveData.kt
+++ app/src/main/java/com/kaano8/androidcore/livedata/MyLiveData.kt
@@ -1,28 +1,55 @@
 package com.kaano8.androidcore.com.kaano8.androidcore.livedata
 
+import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
+import java.util.concurrent.ConcurrentHashMap
 
 class MyLiveData<T> {
+    private val observers = ConcurrentHashMap<(T?) -> Unit, LifecycleObserver>()
 
-    private var observers: HashMap<(T?) -> Unit, LifecycleOwner> = hashMapOf()
-
-    var value: T? = null
+    private var value: T? = null
         set(value) {
             field = value
-            for ((observer, owner) in observers) {
-                if (owner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED))
+
+            for ((observer, lifecycleObserver) in observers) {
+                if (lifecycleObserver.owner.lifecycle.currentState.isAtLeast(Lifecycle.State.STARTED))
                     observer.invoke(value)
             }
         }
 
-    fun observe(observer: (T?) -> Unit, owner: LifecycleOwner) {
-        observers[observer] = owner
+    fun getValue(): T? = value
+
+    fun observe(lifecycleOwner: LifecycleOwner, observer: (T?) -> Unit) {
+        val lifecycleObserver = LifecycleObserver(lifecycleOwner, observer)
+        lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+        observers[observer] = lifecycleObserver
     }
 
     fun removeObserver(observer: (T?) -> Unit) {
-        observers.remove(observer)
+        val lifecycleObserver = observers.remove(observer)
+        lifecycleObserver?.owner?.lifecycle?.removeObserver(lifecycleObserver)
     }
 
+    private fun notifyChange(lifecycleObserver: LifecycleObserver) {
+        lifecycleObserver.observer.invoke(value)
+    }
 
+    private inner class LifecycleObserver(val owner: LifecycleOwner, val observer: (T?) -> Unit): DefaultLifecycleObserver {
+        override fun onStart(owner: LifecycleOwner) {
+            super.onStart(owner)
+            notifyChange(this)
+        }
+
+        override fun onResume(owner: LifecycleOwner) {
+            super.onResume(owner)
+            notifyChange(this)
+        }
+
+        override fun onDestroy(owner: LifecycleOwner) {
+            super.onDestroy(owner)
+            removeObserver(observer)
+        }
+    }
 }
